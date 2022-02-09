1e54922e3eb29bae6b5d8e8444f5d7bcd610d598
asqw887 <asqw887@gmail.com>
2022-02-09 15:06:08 +0900

    init, TodayDate 함수 작성

---
+++ Date/Date/MyCalendar.swift
@@ -0,0 +1,38 @@
+//
+//  MyCalendar.swift
+//  Date
+//
+//  Created by 최예주 on 2022/02/09.
+//
+
+
+// Date 모듈 공부하기
+// 프로그램 구조 설계하기.
+// 함수작성
+
+
+
+import Foundation
+
+class MyCalendar{
+    
+    
+    //오늘 날짜의 년, 월, 일, 시, 분, 초 숫자만 포함하는 객체로 리턴하는 함수
+    func todayDate() -> Date {
+        return Date()
+    }
+    
+    //년월일 날짜만 문자열로 입력하면 Date 인스턴스를 리턴하는 함수
+    // YYYY.MM.DD
+    
+    func stringToDate(_ str: String) -> Date {
+        
+        let dateString = str.split(separator: ".")
+        let dateComponent = DateComponents(year:date[0],
+        
+        return
+    }
+    
+    
+    
+}
---
+++ Date/Date/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Date
+//
+//  Created by 최예주 on 2022/02/09.
+//
+
+import Foundation
+
+let cal = MyCalendar()
+print(cal.todayDate())
+
